bde5c339ce3a2bb9b199e3ceb1dcddc285bebab8
Jona <diego.telenchana6241@utc.edu.ec>
2025-06-29 21:50:15 -0500

    Migraciones

--- Aplicaciones/Equipos/apps.py
+++ Aplicaciones/Equipos/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class EquiposConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Equipos'
+    name = 'Aplicaciones.Equipos'
