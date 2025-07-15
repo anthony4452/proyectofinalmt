ee6fbc1dd4df626224e36c7a28a98b6780fee529
Jona <diego.telenchana6241@utc.edu.ec>
2025-07-14 22:42:38 -0500

    ViewsEstadio

--- Aplicaciones/Equipos/urls.py
+++ Aplicaciones/Equipos/urls.py
@@ -1,8 +1,11 @@
-# Ejemplo: Aplicaciones/Jugadores/urls.py
+# Aplicaciones/Equipos/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listaequipos, name='equipos'),
-    # otras rutas...
+    path('', views.equipo, name='equipos'),  # /equipos/
+    path('equipo', views.equipo),
+    path('nuevoEquipo', views.nuevoEquipo),
+    path('eliminarEquipo/<id>/', views.eliminarEquipo),
+    path('editarEquipo/<id>/', views.editarEquipo),
 ]
--- Aplicaciones/Equipos/views.py
+++ Aplicaciones/Equipos/views.py
@@ -1,6 +1,48 @@
-# Aplicaciones/Jugadores/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Equipo
+from .forms import EquipoForm
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-def listaequipos(request):
-    # lógica
-    return render(request, 'equipos.html')
+# Función para validar si el usuario es admin
+def es_admin(user):
+    return hasattr(user, 'perfilusuario') and user.perfilusuario.rol == 'admin'
+
+# Mostrar lista de equipos (solo admin)
+@login_required
+@user_passes_test(es_admin)
+def equipo(request):
+    equipos = Equipo.objects.all()
+    return render(request, 'equipos.html', {'equipos': equipos})
+
+# Crear nuevo equipo (solo admin)
+@login_required
+@user_passes_test(es_admin)
+def nuevoEquipo(request):
+    if request.method == 'POST':
+        form = EquipoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/equipos/')
+    else:
+        form = EquipoForm()
+    return render(request, 'nuevoEquipo.html', {'form': form})
+
+# Editar equipo existente (solo admin)
+@login_required
+@user_passes_test(es_admin)
+def editarEquipo(request, id):
+    equipo = get_object_or_404(Equipo, id=id)
+    if request.method == 'POST':
+        form = EquipoForm(request.POST, request.FILES, instance=equipo)
+        if form.is_valid():
+            form.save()
+            return redirect('/equipos/')
+    else:
+        form = EquipoForm(instance=equipo)
+    return render(request, 'editarEquipo.html', {'form': form, 'equipo': equipo})
+@login_required
+@user_passes_test(es_admin)
+def eliminarEquipo(request, id):
+    equipo = get_object_or_404(Equipo, id=id)
+    equipo.delete()
+    return redirect('/equipos/')
